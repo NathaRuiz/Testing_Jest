4d8a35b0ff1b4554b4083f930b45a2fc1309a554
NathaRuiz <nruizm98@gmail.com>
2023-12-20 09:31:11 +0100

    update return in validar primos

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,9 +1,7 @@
 class FizzBuzz {
     validar_primos_3_5(numero) {
-        return (numero % 3 === 0 && numero % 5 === 0) ? "FizzBuzz" :
-               (numero % 3 === 0) ? "Fizz" :
-               (numero % 5 === 0) ? "Buzz" :
-               numero;
+        return (numero % 3 !== 0 && numero % 5 !== 0) ? numero : (numero % 3 === 0 && numero % 5 === 0) ? "FizzBuzz" :
+               (numero % 3 === 0) ? "Fizz" : "Buzz";
     }
 }
 
